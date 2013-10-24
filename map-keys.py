a3619e6de1e737a8e79250b5a41ea5fa17f452ca
Elita Baldridge <elita.baldridge@weecology.org>
2013-10-24 15:37:32 -0600

    Adds sketch of function to get list of dbf files from the maps subdirectory.

--- map-keys.py
+++ map-keys.py
@@ -2,6 +2,8 @@
 
 # Import modules
 import csv
+import glob
+import os
 import string
 from dbfpy import dbf
 
@@ -14,6 +16,20 @@ def output_data(filename, header, data):
     output_file.close()
     return filename
 
+# Get list of .dbf files in map directory
+def file_list(folder_path):
+    for root, dirs, files in os.walk(folder_path):
+        for filename in files:
+            if files.endswith(".dbf"):
+                print os.path.join(root, filename)
+                return map_list
+
+
+#Set up file directory parameters
+mammals_folder:(["/data/Mammals_3.0"])
+birds_folder:(["/data/Passeriformes"])
+
+
 # Set up output parameters
 key_header = (['Family', 'Genus','Species', 'Filename'])
 mammal_key_filename = 'mammal_maps_key.csv'
@@ -21,8 +37,14 @@ bird_key_filename = 'bird_maps_key.csv'
 
 
 # Read dbf and write family, genus, species into file
-db = dbf.Dbf(data//order/family/mapname.dbf)
-rec = string.split(db[2])
+def make_key(filename):
+    db = dbf.Dbf(filename)
+    rec = string.split(db[2])
+    return key
+
+# Call file list function
+mammal_file_list = file_list(mammals_folder)
+bird_file_list = file_list(birds_folder)
 
 # Call data output function
 output_data(mammal_key_filename, key_header, mammal_key_data)
