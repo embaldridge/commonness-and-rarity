dffbe6b4429539c125e3e3e5de43486c0d6fcc6d
Elita Baldridge <elita.baldridge@weecology.org>
2013-10-24 16:28:19 -0600

    Uses glob to get paths of dbf files for birds and mammals.  Still working on extracting the data from the dbf files.  The files open, though.

--- map-keys.py
+++ map-keys.py
@@ -16,37 +16,30 @@ def output_data(filename, header, data):
     output_file.close()
     return filename
 
+# Read dbf and write family, genus, species into file
+def make_key(filenames):
+    key = []
+    for files in filenames:
+        db = dbf.Dbf(files)
+        print(db[0])
+        rec = db[0]
+        key = key + rec
+        return key
+
 # Get list of .dbf files in map directory
-def file_list(folder_path):
-    for root, dirs, files in os.walk(folder_path):
-        for filename in files:
-            if files.endswith(".dbf"):
-                print os.path.join(root, filename)
-                return map_list
-
-
-#Set up file directory parameters
-mammals_folder = '/data/Mammals_3.0'
-birds_folder = '/data/Passeriformes'
-
+mammal_files = glob.glob('data/Mammals_3.0/*/*.dbf')
+bird_files = glob.glob('data/Passeriformes/*/*.dbf')
 
 # Set up output parameters
 key_header = (['Family', 'Genus','Species', 'Filename'])
 mammal_key_filename = 'mammal_maps_key.csv'
 bird_key_filename = 'bird_maps_key.csv'
 
-
-# Read dbf and write family, genus, species into file
-def make_key(filename):
-    db = dbf.Dbf(filename)
-    rec = string.split(db[2])
-    return key
-
-# Call file list function
-mammal_file_list = file_list(mammals_folder)
-bird_file_list = file_list(birds_folder)
+# Call map key function
+mammal_key_data = make_key(mammal_files)
+print(mammal_key_data)
+bird_key_data = make_key(bird_files)
 
 # Call data output function
-output_data(mammal_key_filename, key_header, mammal_key_data)
-
-output_data(bird_key_filename, key_header, bird_key_data)  
+#output_data(mammal_key_filename, key_header, mammal_key_data)
+#output_data(bird_key_filename, key_header, bird_key_data)  
