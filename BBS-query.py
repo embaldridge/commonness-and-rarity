a328f1b90d506e7d350b9cb4f256139d8eeec460
Elita Baldridge <elita.baldridge@weecology.org>
2013-10-27 08:44:42 -0600

    Working query with postgresql, extracts data from BBS database with lightning speed.

--- BBS-query.py
+++ BBS-query.py
@@ -1,8 +1,9 @@
 """Script to extract data for chapter 1 from databases"""
 # Import modules
 import csv
-import sqlite3 as dbapi
 import string
+import psycopg2
+import getpass
 
 #Write outputs to .csv file 
 def output_data(filename, header, data):
@@ -13,27 +14,45 @@ def output_data(filename, header, data):
     output_file.close()
     return filename
 
+""" Get password for postgresql"""
+key = getpass.getpass()
+
+
 """ Set up database capabilities """
 # Set up ability to query data
-con = dbapi.connect('data/bbsdb.sqlite')
+con_string = "host='localhost' dbname='bbs' user='postgres' password=" + key
+con = psycopg2.connect(con_string)
 cur = con.cursor()
-
-# Switch con data type to string
-con.text_factory = str
 
 # Query to extract BBS data for analysis
 # (BBS_tablename.statenum * 1000 + BBS_tablename.route) creates a unique identifier for the BBS routes.
 # BBS_species.id_to_species = 1 selects for records that have been identified to species- excludes uncertain ID records.
 # Countrynum = 840 selects for United States records.
 # RPID = 101 selects for standard routes.
-sql_query = cur.execute("""SELECT (BBS_weather.statenum * 10000000 + BBS_weather.route * 10000 + BBS_weather.year) AS RouteYear, (BBS_counts.statenum * 1000 + BBS_counts.route) AS RouteID, BBS_species.sporder AS SpOrder, BBS_species.genus AS Genus, BBS_species.species AS Species, BBS_routes.lati AS latitude, BBS_routes.loni AS longitude, BBS_counts.Aou AS AOU, BBS_counts.SpeciesTotal AS Abundance FROM BBS_counts
-    JOIN BBS_species 
-    ON BBS_counts.Aou == BBS_species.AOU
-    JOIN BBS_routes
-    ON (BBS_counts.statenum * 1000 + BBS_counts.route) == (BBS_routes.statenum * 1000 + BBS_routes.route)
-    JOIN BBS_weather
-    ON (BBS_counts.statenum * 10000000 + BBS_counts.route * 10000 + BBS_counts.year) == (BBS_weather.statenum * 10000000 + BBS_weather.route * 10000 + BBS_weather.year)
-    WHERE BBS_counts.countrynum == "840" AND BBS_counts.RPID == "101" AND BBS_species.sporder == "Passeriformes" AND BBS_counts.YEAR == "2005";""")
+sql_query = cur.execute("""SELECT 
+    (weather.statenum * 10000000 + weather.route * 10000 + weather.year) AS RouteIDYear,
+    (counts.statenum * 1000 + counts.route) AS RouteID, 
+    species.sporder AS SpOrder,
+    species.family AS Family,
+    species.genus AS Genus, 
+    species.species AS Species, 
+    routes.lati AS latitude, 
+    routes.loni AS longitude, 
+    counts.Aou AS AOU, 
+    counts.SpeciesTotal AS Abundance 
+FROM 
+    bbs.counts,
+    bbs.species,
+    bbs.routes,
+    bbs.weather
+WHERE
+    counts.Aou = species.AOU AND
+    (counts.statenum * 1000 + counts.route) = (routes.statenum * 1000 + routes.route) AND
+    (counts.statenum * 10000000 + counts.route * 10000 + counts.year) = (weather.statenum * 10000000 + weather.route * 10000 + weather.year) AND
+    counts.countrynum = 840 AND 
+    counts.RPID = 101 AND 
+    species.sporder = 'Passeriformes' AND 
+    counts.YEAR = 2005;""")
 
 BBS_data = cur.fetchall()
 
