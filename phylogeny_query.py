ec65c81784fad96e39e59f30bfdb402be065c434
Elita Baldridge <elita.baldridge@weecology.org>
2013-11-18 15:46:28 -0700

    Adds functions for importing and exporting .csv files.

--- phylogeny_query.py
+++ phylogeny_query.py
@@ -6,6 +6,27 @@ import string
 import getpass
 import psycopg2
 
+#Import data from a .csv file
+def import_data(filename):
+    datafile = open(filename, 'r')
+    datareader = csv.reader(datafile, delimiter=',')
+    
+    data = []
+    
+    for row in datareader:
+        data.append(row)
+
+    return data
+
+#Write outputs to .csv file 
+def output_data(filename, header, data):
+    output_file = open(filename, "wb")
+    datawriter = csv.writer(output_file)
+    datawriter.writerow(header)
+    datawriter.writerows(data)
+    output_file.close()
+    return filename
+
 #Get species names from files, put into web query, get pruned tree.
 
 
