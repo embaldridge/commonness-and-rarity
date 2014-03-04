118c817966bfef2b6c8c324e42e79b7a3c12c220
Elita Baldridge <elita.baldridge@weecology.org>
2014-03-04 06:32:21 -0600

    Adds spatial-query.py to repository.

---
+++ spatial-query.py
@@ -0,0 +1,7 @@
+""" Takes geographic ranges for each species, determines what ranges could potentially occur there."""  
+
+# Import modules
+import csv
+import string
+import psycopg2
+import getpass
