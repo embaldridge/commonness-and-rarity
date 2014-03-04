a4671b5549aca2d835984a97b0518e041cec25a3
Elita Baldridge <elita.baldridge@weecology.org>
2014-03-04 08:37:39 -0600

    Adds beginning of web query for phylogenetic data from PhyloCommons.  Has bits of Ben's sample gist still in there for reference.

--- phylogeny_query.py
+++ phylogeny_query.py
@@ -34,5 +34,19 @@ mcdb_filename = 'MCDB_extracted.csv'
 BBS_data = import_data(BBS_filename)
 mcdb_data = import_data(mcdb_filename)
 
+# Query BBS data.
+base_url= 'http://phylocommons.org/query/prune=True&format=newick
+BBS_query_url&taxa='
+mammal_tree_name= '&tree=bininda-emonds_mammals'
+
+= 'http://phylocommons.org/query/prune=True&format=newick&taxa=Homo+sapiens%2CGorilla+gorilla%2CPan+paniscus%2CPan+troglodytes&tree=bininda-emonds_mammals'
+ 
+result = urllib2.urlopen(my_query_url)
+tree = result.read()
+
+output_data(tree)
+
+
+
 
 
