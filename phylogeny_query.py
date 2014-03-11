bfde398fad220174478457f00d18959b85b3df7d
Elita Baldridge <elita.baldridge@weecology.org>
2014-03-11 15:14:38 -0500

    I have no idea what changes I made between this commit and the last one, but apparently I made some.  I have no recollection of this whatsoever, and I am hoping it is a return or a comment or something.

--- phylogeny_query.py
+++ phylogeny_query.py
@@ -30,65 +30,13 @@ def output_data(filename, header, data):
     output_file.close()
     return filename
 
-# Extract species names from files, put into string.
-def PhyloCommons_species_list(species_list_file, tree_name):
-    full_tree = ['genus, species, branch_length']
-           
-    species_list = import_data(species_list_file)
-    
-    species_url_list = ['http://phylocommons.org/query/prune=True&format=newick&taxa=']
-    
-    for record in species_list:
-        if len(species_url_list) >= 10:
-            #Get genus, species
-            species_url_list.append(record[3] + '+'
-        + record[4] + '%2C' + tree_name) 
-               
-  
-            species_url = ''.join(map(str, species_url_list))
-            species_url.strip
-            print(species_url)
-    
-            tree_result = urllib2.urlopen(species_url)
-            tree = tree_result.read()
-        
-            print(tree)
-        
-            full_tree = full_tree.append(tree)
-            
-            species_url_list = ['http://phylocommons.org/query/prune=True&format=newick&taxa=']
-        
-        else:
-            species_url_list.append(record[3] + '+'
-                    + record[4] + '%2C')            
-        
-        
-        species_url_list.append(tree_name)
-        
-    return full_tree   
+# Import trees.
+mammal_tree = Phylo.read("data/mammal-supertree.tre", "newick")
+bird_tree = Phylo.read("data/AllBirdsHackett1.tre", "newick")
 
 #Get species names from files, put into web query, get pruned tree.
 BBS_filename = 'BBS_extracted.csv'
 mcdb_filename = 'MCDB_extracted.csv'
-
-mammal_tree_name = '&tree=bininda-emonds_mammals'
-bird_tree_name = '&tree=jetz_birds'
-
-# Get URLs.
-BBS_URL = PhyloCommons_species_list(BBS_filename, bird_tree_name)
-MCDB_URL = PhyloCommons_species_list(mcdb_filename, mammal_tree_name)
-
-
-BBS_tree_result = urllib2.urlopen(BBS_URL)
-BBS_tree = BBS_tree_result.read()
-
-
-mcdb_tree_result = urllib2.urlopen(MCDB_URL)
-mcdb_tree = mcdb_tree_result.read()
-
-
-output_data(BBS_tree)
-output_data(mcdb_tree)
 
 
 
